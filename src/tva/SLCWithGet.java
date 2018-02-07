9d343df75f51b15769ed63476caa0f67914c5cdf
Hugormen <hugo.ekelund@telia.com>
2018-02-07 11:49:10 +0100

    add get -method and work on add

--- src/tva/SLCWithGet.java
+++ src/tva/SLCWithGet.java
@@ -1,5 +1,7 @@
 package tva;
 
+import java.util.NoSuchElementException;
+
 public class SLCWithGet<E extends Comparable<? super E>> extends LinkedCollection<E> implements CollectionWithGet<E> {
 
 
@@ -18,17 +20,30 @@ public class SLCWithGet<E extends Comparable<? super E>> extends LinkedCollectio
         if (element == null)
             throw new NullPointerException();
         else if (this.isEmpty()) {
-            head = new Entry( element, head );
+            head = new Entry(element, head);
+            return true;
+        }else if(element.compareTo(head.element)<=0){
+            Entry newEntry = new Entry(element,head);
+            head = newEntry;
             return true;
         }else{
+            Entry latestEntry = head;
             for ( Entry p = head; p != null; p = p.next ){
-                if (element.compareTo(p))
+                if (element.compareTo(p.element)<0){
+
+                }
             }
         }
     }
 
     @Override
     public E get(E e) {
-        return null;
+        for ( Entry p = head; p != null; p = p.next ){
+            if (e == p){
+                return p.element;
+            }else{
+                throw new NoSuchElementException();
+            }
+        }
     }
 }
