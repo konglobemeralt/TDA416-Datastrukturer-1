063a5678c1c40de9bade86fa37279967f6cf6cf0
Emil Jansson <emiljanQ3@hotmail.com>
2018-02-08 14:39:57 +0100

    Finished the SplayTree, untested

--- src/tva/SplayWithGet.java
+++ src/tva/SplayWithGet.java
@@ -4,37 +4,165 @@ public class SplayWithGet<E extends Comparable<? super E>> extends BinarySearchT
 
     @Override
     public E get(E element) {
+        if (element == null){
+            throw new NullPointerException();
+        }
         Entry searchedEntry = this.find(element, this.root);
 
-        while (searchedEntry != root){  //TODO Null checks
-            if (searchedEntry.element.compareTo(searchedEntry.parent.element) > 1){
+        rearrangeToNewRoot(searchedEntry);
+
+        this.root = searchedEntry;
+
+        return element;
+    }
+
+
+    private void rearrangeToNewRoot(Entry newRoot){
+        while (newRoot.parent.parent != null){  //Check if gp exists
+            if (newRoot.element.compareTo(newRoot.parent.element) > 0){
                 //parent is smaller
-                if(searchedEntry.element.compareTo(searchedEntry.parent.parent.element) > 1){
+                if(newRoot.element.compareTo(newRoot.parent.parent.element) > 0){
                     //gp is smaller
-                    //TODO preform zig
+                    zig(newRoot.parent);
                 }else{
                     //gp is larger
-                    //TODO preform zigzag
+                    zigZag(newRoot.parent.parent);
                 }
-            }else if(searchedEntry.element.compareTo(searchedEntry.parent.element) < 1){
+            }else if(newRoot.element.compareTo(newRoot.parent.element) < 0){
                 //parent is larger
-                if(searchedEntry.element.compareTo(searchedEntry.parent.parent.element) > 1){
+                if(newRoot.element.compareTo(newRoot.parent.parent.element) > 0){
                     //gp is smaller
-                    //TODO preform zagzig
+                    zagZig(newRoot.parent.parent);
                 }else{
                     //gp is larger
-                    //TODO preform zag
+                    zag(newRoot.parent);
                 }
             }else{
-                //same size !? TODO What todo?
+                rearrangeWhenEqualParent(newRoot);
             }
         }
 
-        // check parent och gp
-        // parent och gp större = zag                       rotateRight( parent )
-        // parent och gp mindre = zig                       rotateLeft( parent )
-        // parent större och gp mindre = zagzig             doubleRotateLeft( gp )
-        // parent mindre och gp större = zigzag             doubleRotateRight( gp )
-        return null;
+        if (newRoot.parent != null){
+            if(newRoot.element.compareTo(newRoot.parent.element) > 0){
+                zig(newRoot.parent);
+            }else if(newRoot.element.compareTo(newRoot.parent.element) > 0){
+                zag(newRoot.parent);
+            }else{
+                rearrangeWhenEqualParent(newRoot);
+            }
+        }
     }
+
+    private void rearrangeWhenEqualParent(Entry child){
+        if (isRightChild(child)){
+            zig(child.parent);
+        }else{
+            zag(child.parent);
+        }
+    }
+
+    private boolean isRightChild(Entry child){
+        return child.parent.right == child;
+    }
+
+
+    /* Rotera 1 steg i hogervarv, dvs
+              x'                 y'
+             / \                / \
+            y'  C   -->        A   x'
+           / \                    / \
+          A   B                  B   C
+    */
+    private void zag( Entry x ) {
+        Entry   y = x.left;
+        E    temp = x.element;
+        x.element = y.element;
+        y.element = temp;
+        x.left    = y.left;
+        if ( x.left != null )
+            x.left.parent   = x;
+        y.left    = y.right;
+        y.right   = x.right;
+        if ( y.right != null )
+            y.right.parent  = y;
+        x.right   = y;
+    } //   rotateRight
+    // ========== ========== ========== ==========
+
+    /* Rotera 1 steg i vanstervarv, dvs
+              x'                 y'
+             / \                / \
+            A   y'  -->        x'  C
+               / \            / \
+              B   C          A   B
+    */
+    private void zig( Entry x ) {
+        Entry  y  = x.right;
+        E temp    = x.element;
+        x.element = y.element;
+        y.element = temp;
+        x.right   = y.right;
+        if ( x.right != null )
+            x.right.parent  = x;
+        y.right   = y.left;
+        y.left    = x.left;
+        if ( y.left != null )
+            y.left.parent   = y;
+        x.left    = y;
+    } //   rotateLeft
+    // ========== ========== ========== ==========
+
+    /* Rotera 2 steg i hogervarv, dvs
+              x'                  z'
+             / \                /   \
+            y'  D   -->        y'    x'
+           / \                / \   / \
+          A   z'             A   B C   D
+             / \
+            B   C
+    */
+    private void zigZag( Entry x ) {
+        Entry   y = x.left,
+                z = x.left.right;
+        E       e = x.element;
+        x.element = z.element;
+        z.element = e;
+        y.right   = z.left;
+        if ( y.right != null )
+            y.right.parent = y;
+        z.left    = z.right;
+        z.right   = x.right;
+        if ( z.right != null )
+            z.right.parent = z;
+        x.right   = z;
+        z.parent  = x;
+    }  //  doubleRotateRight
+    // ========== ========== ========== ==========
+
+    /* Rotera 2 steg i vanstervarv, dvs
+               x'                  z'
+              / \                /   \
+             A   y'   -->       x'    y'
+                / \            / \   / \
+               z   D          A   B C   D
+              / \
+             B   C
+     */
+    private void zagZig( Entry x ) {
+        Entry  y  = x.right,
+                z  = x.right.left;
+        E      e  = x.element;
+        x.element = z.element;
+        z.element = e;
+        y.left    = z.right;
+        if ( y.left != null )
+            y.left.parent = y;
+        z.right   = z.left;
+        z.left    = x.left;
+        if ( z.left != null )
+            z.left.parent = z;
+        x.left    = z;
+        z.parent  = x;
+    } //  doubleRotateLeft
+
 }
